9655d20d13a55064f8f5b79aeea2b001b57ff2ec
Abu Talha Tamim <talha@boishakhikunja.onmicrosoft.com>
2025-06-02 16:43:15 +0600

    Input Bug Fix Needed

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,13 +1,23 @@
 // DOM list
+const assignNull = document.querySelector(".assign-null");
+const assignTask = document.querySelector(".assign-task");
 const taskInput = document.querySelector(".task-input");
 const toDoList = document.querySelector(".to-do-list");
-const assignNull = document.querySelector(".assign-null");
 
 const handleAddTask = function () {
   if (!taskInput.value) {
     assignNull.innerHTML = `You can't do nothing. There's always something to do`;
+    assignTask.style = "outline: 0.2rem solid red; border-radius: 1rem;";
   } else {
     assignNull.innerHTML = ``;
-    console.log(taskInput.value);
+    assignTask.style = "outline: none;";
+    // Creating Single To Do
+    const createSingleToDo = document.createElement("div"); // Create Tags
+    toDoList.appendChild(createSingleToDo); // Append Child element
+    createSingleToDo.classList.add("single-to-do"); // Adding Class Name
+
+    // Creating Input
+    const showInput = document.querySelector("input"); // Create Tags
+    createSingleToDo.appendChild(showInput); // Append Child element
   }
 };
